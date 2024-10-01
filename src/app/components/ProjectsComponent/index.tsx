db501554dd88377d5504e57ecd0ba6e0be083e69
luiz <luizalthmandev@gmail.com>
2024-10-01 01:16:51 -0300

    fix image background

--- src/app/components/ProjectsComponent/index.tsx
+++ src/app/components/ProjectsComponent/index.tsx
@@ -9,7 +9,7 @@ export function ProjectCard({ image, title, description }: ProjectCardType) {
                 alt={title}
                 className="w-full aspect-[4/3] object-cover transition-transform duration-300"
             />
-            <div className="absolute m-5 inset-0 flex items-center justify-center sm:opacity-0 sm:group-hover:opacity-90 opacity-90 transition-opacity duration-300 bg-[var(--white)] bg-opacity-70 p-5">
+            <div className="absolute m-3 inset-0 flex items-center justify-center sm:opacity-0 sm:group-hover:opacity-90 opacity-90 transition-opacity duration-300 bg-[var(--white)] bg-opacity-70 p-5">
                 <div>
                     <h3 className="font-bold">{title}</h3>
                     <p className="mt-2">{description}</p>
